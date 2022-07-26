605c4a7a4ef74f374e6566d2dd1f0e84a1b068ea
unknown <ibr.s.ahmat@gmail.com>
2022-07-27 00:59:52 +0300

    Add super for the class

--- person.rb
+++ person.rb
@@ -1,11 +1,11 @@
 require './nameable'
 
 class Person < Nameable
-
   attr_reader :id # we need only getter
   attr_accessor :name, :age # we need to get and set
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -17,7 +17,6 @@ class Person < Nameable
   end
   private :of_age?
 
-  public
   def can_use_services?
     of_age? || @parent_permission
   end
