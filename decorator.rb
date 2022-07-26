9f982768bad10c95d8e6a0b9991216ab68009358
unknown <ibr.s.ahmat@gmail.com>
2022-07-27 00:29:45 +0300

    Create Decorator class inherits from Nameable class

---
+++ decorator.rb
@@ -0,0 +1,14 @@
+require './nameable'
+
+class Decorator < Nameable
+  attr_accessor :name
+
+  def initialize(name)
+    @nameable = name
+    super
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
