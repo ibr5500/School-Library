393c16ace5564ad2fc68a75f779c59a5e972cd77
unknown <ibr.s.ahmat@gmail.com>
2022-07-28 11:53:10 +0300

    Create the has-many relationship

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './nameable'
 
 class Person < Nameable
-  attr_reader :id # we need only getter
+  attr_reader :id, :rentals # we need only getter
   attr_accessor :name, :age # we need to get and set
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
@@ -23,5 +24,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def rent_book(book, person)
+    Rental.new(book, person, self)
   end
 end
