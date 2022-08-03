481b5c1e48edb0f543ad264e33da42a3d8c183a0
unknown <ibr.s.ahmat@gmail.com>
2022-08-03 16:16:36 +0300

    Preserve data in Json files

--- rental.rb
+++ rental.rb
@@ -4,7 +4,7 @@ class Rental
   attr_reader :book, :person
   attr_accessor :date
 
-  def initialize(date, person, book)
+  def initialize(date, book, person)
     @date = date
 
     @book = book
--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name, classroom = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
